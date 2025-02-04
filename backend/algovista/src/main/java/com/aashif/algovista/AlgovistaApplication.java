392be2fb6154213cb1b8e2d38944975f84066d2d
Aashif Sajah <masaashifsajah@gmail.com>
2025-02-04 22:34:35 +0530

    Added Frontend and Backend Projects

---
+++ backend/algovista/src/main/java/com/aashif/algovista/AlgovistaApplication.java
@@ -0,0 +1,13 @@
+package com.aashif.algovista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlgovistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlgovistaApplication.class, args);
+	}
+
+}
